86d7c1fcfbf2c718ecc60b07f10e8d78175d9684
rangerjohn99 <rangerjohn99@gmail.com>
2025-04-01 15:25:43 -0500

    small edit

--- github.R
+++ github.R
@@ -1,3 +1,7 @@
+# download git
+# https://rfortherestofus.com/2021/02/how-to-use-git-github-with-r
+
+# configure git
 library(usethis)
 
 use_git_config(user.name = "rangerjohn99", user.email = "rangerjohn99@gmail.com")
@@ -5,6 +9,8 @@ use_git_config(user.name = "rangerjohn99", user.email = "rangerjohn99@gmail.com"
 use_git()
 
 create_github_token()
+
+# need to request github token
 
 library(gitcreds)
 
