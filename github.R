d1dc638a9296b7a61322a5b7198ea9af3271a0d3
rangerjohn99 <rangerjohn99@gmail.com>
2025-04-01 12:37:29 -0500

    moved non-R stuff and added github set up code

---
+++ github.R
@@ -0,0 +1,13 @@
+library(usethis)
+
+use_git_config(user.name = "rangerjohn99", user.email = "rangerjohn99@gmail.com")
+
+use_git()
+
+create_github_token()
+
+library(gitcreds)
+
+gitcreds_set()
+
+use_github()
